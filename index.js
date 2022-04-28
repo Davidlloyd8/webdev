7a5ee18a682b8eaf848584e6fcd8fae31735e7d9
Davidlloyd8 <extendavidba4@gmail.com>
2022-04-28 23:40:53 +0100

    Creating the navbar

---
+++ index.js
@@ -0,0 +1,18 @@
+const icon_button = document.querySelector(".menu-icon");
+const navbarMenu = document.querySelector(".navbar-links");
+
+icon_button.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  icon_button.classList.toggle("active");
+  navbarMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll("#nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  icon_button.classList.remove("active");
+  navbarMenu.classList.remove("active");
+}
